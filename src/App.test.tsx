24b0b72a3c2c4f88a35256799a4bb7b083b37aad
MFukazawa <micahfukazawa@gmail.com>
2021-07-21 14:30:45 +0900

    Add more tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,58 @@
 import React from 'react';
+import axios from 'axios'
 import { render, screen } from '@testing-library/react';
-import App from './App';
-import sum from './math'
+import userEvent from '@testing-library/user-event'
+// import sum from './math'
 
-describe('sum', () => {
-  test('sums up two values', () => {
-    expect(sum(2, 4)).toBe(6);
-  })
-})
+import App from './App';
+
+// const mockedAxios = axios as jest.Mocked<typeof axios>;
+jest.mock('axios')
 
 describe('App', () => {
-  test('renders App component', () => {
-    render(<App />)
+  test('fetches stories from an API and displays them', async () => {
+    const stories = [
+      { objectID: '1', title: 'Hello' },
+      { objectID: '2', title: 'React' },
+    ];
 
-    screen.debug();
-  })
-})
+    axios.get.mockImplementationOnce(() =>
+      Promise.resolve({ data: { hits: stories } })
+    );
+
+    render(<App />);
+
+    await userEvent.click(screen.getByRole('button'));
+
+    const items = await screen.findAllByRole('listitem');
+
+    expect(items).toHaveLength(2);
+  });
+});
+
+// describe('sum', () => {
+//   test('sums up two values', () => {
+//     expect(sum(2, 4)).toBe(6);
+//   })
+// })
+
+// describe('App', () => {
+//   test('renders App component', () => {
+//     render(<App />)
+
+//     screen.debug();
+//     // expect(screen.getByRole('textbox')).toBeInTheDocument()
+
+//     fireEvent.change(screen.getByRole('textbox'), {
+//       target: { value: 'JavaScript' }
+//     })
+//   })
+// })
+
+// test('render textbox', () => {
+//   render(<App/>)
+//   expect(screen.getByRole('textbox')).toBeInTheDocument()
+// })
 
 // test('renders learn react link', () => {
 //   render(<App />);
@@ -23,8 +60,23 @@ describe('App', () => {
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-test('renders search text', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Searches for/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders search text', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/Searches for/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// test('check div that does not exist', () => {
+//   render(<App/>)
+//   expect(screen.queryByText(/This shouldn't exist/)).toBeNull()
+// })
+
+// describe('Async', () => {
+//   test('renders App component', async () => {
+//     render(<App/>)
+
+//     expect(screen.queryByText(/Signed in as/)).toBeNull();
+
+//     expect(await screen.findByText(/Signed in as/)).toBeInTheDocument();
+//   })
+// })
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,107 @@
 import React from 'react';
+import axios from 'axios';
 
-type ISearch = {
-  value: string
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
-  // onChange: any,
-  children: React.ReactChildren[] | React.ReactChild
-}
+const URL = 'http://hn.algolia.com/api/v1/search';
 
 function App() {
-  const [search, setSearch] = React.useState('');
+  const [stories, setStories] = React.useState([]);
+  const [error, setError] = React.useState(null);
 
-  function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
-    setSearch(event.target.value);
+  async function handleFetch(event) {
+    let result;
+
+    try {
+      result = await axios.get(`${URL}?query=React`);
+
+      setStories(result.data.hits);
+    } catch (error) {
+      setError(error);
+    }
   }
 
   return (
     <div>
-      <Search value={search} onChange={handleChange}>
-        Search:
-      </Search>
+      <button type="button" onClick={handleFetch}>
+        Fetch Stories
+      </button>
 
-      <p>Searches for {search ? search : '...'}</p>
-    </div>
-  );
-}
+      {error && <span>Something went wrong ...</span>}
 
-function Search({ value, onChange, children }: ISearch): JSX.Element {
-  return (
-    <div>
-      <label htmlFor="search">{children}</label>
-      <input
-        id="search"
-        type="text"
-        value={value}
-        onChange={onChange}
-      />
+      <ul>
+        {stories.map((story) => (
+          <li key={story.objectID}>
+            <a href={story.url}>{story.title}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
 export default App;
+
+// import React, { useEffect, useState } from 'react';
+
+// type SearchTypes = {
+//   value: string
+//   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+//   children: React.ReactChildren[] | React.ReactChild
+// }
+
+// type UserTypes = {
+//   id: string,
+//   name: string
+// }
+
+// function getUser() {
+//   return Promise.resolve({ id: '1', name: 'Robin' });
+// }
+
+// function App() {
+//   const [search, setSearch] = useState('');
+//   const [user, setUser] = useState<UserTypes | null>(null);
+
+//   useEffect(() => {
+//     const loadUser = async () => {
+//       const user = await getUser();
+//       setUser(user);
+//     };
+
+//     loadUser();
+//   }, []);
+
+//   function handleChange(event: React.ChangeEvent<HTMLInputElement>): void {
+//     setSearch(event.target.value);
+//   }
+
+//   return (
+//     <div>
+//       {user ? <p>Signed in as {user.name}</p> : null}
+
+//       <Search value={search} onChange={handleChange}>
+//         Search:
+//       </Search>
+
+//       <p>Searches for {search ? search : '...'}</p>
+//     </div>
+//   );
+// }
+
+// function Search({ value, onChange, children }: SearchTypes): JSX.Element {
+//   return (
+//     <div>
+//       <label htmlFor="search">{children}</label>
+//       <input
+//         id="search"
+//         type="text"
+//         value={value}
+//         onChange={onChange}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
 
 // import React from 'react';
 // import logo from './logo.svg';
